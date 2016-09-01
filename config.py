d4eb83e348ea05dd42c9a135215ee3128a9ab070
Renzf2015 <328713040@qq.com>
2016-09-01 12:58:29 +0800

    v1.1.7

--- config.py
+++ config.py
@@ -54,7 +54,7 @@ class ProductionConfig(Config):
             credintials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
             if getattr(cls, 'MAIL_USE_TLS', None):
                 secure = ()
-        mail_handler = STMPHandler(mailhost = (cls.MAIL_SERVER,cls.MAIL_PORT),
+        mail_handler = SMTPHandler(mailhost = (cls.MAIL_SERVER,cls.MAIL_PORT),
             fromaddr =cls.FLASKY_MAIL_SENDER,
             toaddrs = [cls.FLASK_ADMIN],
             subject = cls.FLASKY_MAIL_SUBJECT_PREFIX + 'App Error',
