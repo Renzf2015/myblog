f32383dd55e4b0dd1bc26d1b94a5240384e59e8f
Renzf2015 <328713040@qq.com>
2016-09-05 23:07:40 +0800

    添加template/posts

--- app/main/views.py
+++ app/main/views.py
@@ -308,5 +308,22 @@ def delete(id):
     db.session.delete(post)
     return redirect(url_for('.index'))
 
+@main.route('/posts')
+def posts():
+    page = request.args.get('page', 1, type=int)
+    show_followed = False
+    if current_user.is_authenticated:
+        show_followed = bool(request.cookies.get('show_followed', ''))
+    if show_followed:
+        query = current_user.followed_posts
+    else:
+        query = Post.query
+    pagination = query.order_by(Post.timestamp.desc()).paginate(
+        page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+        error_out=False)
+    posts = pagination.items
+    return render_template('posts.html', posts=posts,
+                           show_followed=show_followed, pagination=pagination)
+
 
 
